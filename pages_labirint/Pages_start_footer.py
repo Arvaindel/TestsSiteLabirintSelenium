e583b5e296f1bbbf6c2dcdaee3bd9ba91a44acb2
Arvaindel <81435344+Arvaindel@users.noreply.github.com>
2021-10-21 21:24:17 +0300

    Page object footer

---
+++ pages_labirint/Pages_start_footer.py
@@ -0,0 +1,45 @@
+from selenium.webdriver.common.keys import Keys
+
+class StartLabFooter():
+    def __init__(self, driver):
+        self.driver=driver
+        self.spisok_social_networks =[]
+        self.spisok = []
+
+    def apply_cookie(self):
+        return self.driver.find_element_by_css_selector('.cookie-policy__button.js-cookie-policy-agree')
+
+    def get_scroll_footer(self):
+        self.driver.find_element_by_tag_name('body').send_keys(Keys.END)
+
+    def get_swith_to_windows(self):
+        self.driver.switch_to_window(self.driver.window_handles[1])
+
+    def get_footer_app_store(self):
+        return self.driver.find_element_by_css_selector(".b-rfooter-e-logo.b-rfooter-e-logo-appstore.analytics-click-js")
+
+    def get_footer_google_play(self):
+        return self.driver.find_element_by_css_selector(".b-rfooter-e-logo.b-rfooter-e-logo-googleplay.analytics-click-js")
+
+    def get_footer_app_galery(self):
+        return self.driver.find_element_by_css_selector(".b-rfooter-e-logo.b-rfooter-e-logo-appgallery.analytics-click-js")
+
+    def get_social_networks_spisok(self):
+        for i in range(len(self.driver.find_elements_by_xpath('//div[contains(text(), "Мы в соцсетях")]//following-sibling::div/ul/li'))):
+            self.spisok_social_networks.append(self.driver.find_elements_by_xpath('//div[contains(text(), "Мы в соцсетях")]//following-sibling::div/ul/li/a')[i])
+        return self.spisok_social_networks
+
+    def get_footer_katalog_books(self):
+        for i in range(len(self.driver.find_elements_by_xpath('//div[contains(text(), "Каталог")]//following-sibling::div/ul/li'))):
+            self.spisok.append(self.driver.find_elements_by_xpath('//div[contains(text(), "Каталог")]//following-sibling::div/ul/li/a')[i])
+        return self.spisok
+
+    def get_footer_important(self):
+        for i in range(len(self.driver.find_elements_by_xpath('//div[contains(text(), "Важно")]//following-sibling::div/ul/li'))):
+            self.spisok.append(self.driver.find_elements_by_xpath('//div[contains(text(), "Важно")]//following-sibling::div/ul/li/a')[i])
+        return self.spisok
+
+    def get_footer_intresting(self):
+        for i in range(len(self.driver.find_elements_by_xpath('//div[contains(text(), "Интересно")]//following-sibling::div/ul/li'))):
+            self.spisok.append(self.driver.find_elements_by_xpath('//div[contains(text(), "Интересно")]//following-sibling::div/ul/li/a')[i])
+        return self.spisok
